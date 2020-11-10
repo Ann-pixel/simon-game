cdecdfc800058cf5eb916d8f6c1eca5b10a143e6
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2020-11-10 17:05:43 -0500

    uploading all files

---
+++ game.js
@@ -0,0 +1,101 @@
+// declaring initial variables
+var buttonColors=["red","blue","green","yellow"];
+
+var userClickedPattern= [];
+ var gamePattern= [];
+
+var started= false;
+var level=0;
+
+// starting the game-
+$(document).click(startGame());
+ $(document).keydown(startGame());
+ 
+ function startGame(){
+    if(!started) {
+      $("#level-title").text("Level "+level);
+
+      nextSequence();
+      started=true;
+    }
+  }
+
+// documenting user clickpattern--
+
+  $(".btn").click(function(){
+    var userChosenColor= $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+
+    checkAnswer(userClickedPattern.length-1);
+  });
+
+
+  // checking for accuracy-
+  function checkAnswer(currentLevel){
+
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+
+      if(gamePattern.length === userClickedPattern.length){
+          setTimeout(function(){nextSequence()},1000);
+
+    }} else{
+
+      playSound("wrong")
+
+      $("body").addClass("game-over");
+      $("h1").text("Game Over, Press Any Key to Restart");
+      setTimeout(function(){
+      $("body").removeClass("game-over"), 500;});
+      startOver();
+    }
+  }
+
+// starting a new level-
+ function nextSequence(){
+   userClickedPattern= [];
+   level++;
+
+   $("#level-title").text("Level "+ level);
+
+  var randomNumber= Math.floor(Math.random()*4);
+   var randomChosenColor= buttonColors[randomNumber];
+   gamePattern.push(randomChosenColor);
+
+   $("#"+randomChosenColor).fadeOut(100).fadeIn(100);
+
+   playSound(randomChosenColor);
+
+ }
+
+
+// restart
+
+function startOver(){
+level=0;
+gamePattern=[];
+started=false;
+}
+
+
+
+
+
+
+//keeping track of user pattern & animations and sound effects-
+
+
+
+function animatePress(currentColor){
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function(){
+    $("#" + currentColor).removeClass("pressed")}, 100);
+  }
+
+
+function playSound(name){
+  var sound= new Audio ("sounds/"+name+".mp3");
+  sound.play();
+}
